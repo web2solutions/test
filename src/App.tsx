7cd9a8d85616ce36644f156fc842c2c01469be68
Eduardo Almeida <web2solucoes@gmail.com>
2021-02-26 01:08:38 -0300

    :)

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+
 import { Redirect, Route } from 'react-router-dom';
 import {
   IonApp,
@@ -6,11 +7,12 @@ import {
   IonRouterOutlet,
   IonTabBar,
   IonTabButton,
-  IonTabs,
+  IonTabs
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import { ellipse, square, triangle } from 'ionicons/icons';
-import Tab1 from './pages/Tab1';
+import { search, square, cafe } from 'ionicons/icons';
+import Drinks from './pages/Drinks';
+import Drink from './pages/Drink';
 import Tab2 from './pages/Tab2';
 import Tab3 from './pages/Tab3';
 
@@ -33,41 +35,46 @@ import '@ionic/react/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
-const App: React.FC = () => (
-  <IonApp>
-    <IonReactRouter>
-      <IonTabs>
-        <IonRouterOutlet>
-          <Route exact path="/tab1">
-            <Tab1 />
-          </Route>
-          <Route exact path="/tab2">
-            <Tab2 />
-          </Route>
-          <Route path="/tab3">
-            <Tab3 />
-          </Route>
-          <Route exact path="/">
-            <Redirect to="/tab1" />
-          </Route>
-        </IonRouterOutlet>
-        <IonTabBar slot="bottom">
-          <IonTabButton tab="tab1" href="/tab1">
-            <IonIcon icon={triangle} />
-            <IonLabel>Tab 1</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab2" href="/tab2">
-            <IonIcon icon={ellipse} />
-            <IonLabel>Tab 2</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab3" href="/tab3">
-            <IonIcon icon={square} />
-            <IonLabel>Tab 3</IonLabel>
-          </IonTabButton>
-        </IonTabBar>
-      </IonTabs>
-    </IonReactRouter>
-  </IonApp>
-);
+const App: React.FC = () => {
+  
+  return (
+    <IonApp>
+      <IonReactRouter>
+        
+        <IonTabs>
+          <IonRouterOutlet>
+            <Route exact path="/drinks">
+              <Drinks />
+            </Route>
+            <Route exact path="/tab2">
+              <Tab2 />
+            </Route>
+            <Route path="/tab3">
+              <Tab3 />
+            </Route>
+            <Route path="/drink/:__id"  component={Drink} />
+            <Route exact path="/">
+              <Redirect to="/drinks" />
+            </Route>
+          </IonRouterOutlet>
+          <IonTabBar slot="bottom">
+            <IonTabButton tab="drinks" href="/drinks">
+              <IonIcon icon={cafe} />
+              <IonLabel>Drinks</IonLabel>
+            </IonTabButton>
+            <IonTabButton tab="tab2" href="/tab2">
+              <IonIcon icon={search} />
+              <IonLabel>Search</IonLabel>
+            </IonTabButton>
+            <IonTabButton tab="tab3" href="/tab3">
+              <IonIcon icon={square} />
+              <IonLabel>Tab 3</IonLabel>
+            </IonTabButton>
+          </IonTabBar>
+        </IonTabs>
+      </IonReactRouter>
+    </IonApp>
+  )
+};
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -4,19 +4,66 @@ import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
+// import voodux
+import voodux from 'voodux'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import { FoundationProvider } from './FoundationContext'
+
+
+// import data schemas
+import UserSchema from './schemas/User'
+import ProductSchema from './schemas/Product'
+import OrderSchema from './schemas/Order'
+import CustomerSchema from './schemas/Customer'
+import DrinkSchema from './schemas/Drink'
+
+// foundation event handlers
+import onApplicationStart from './events/onApplicationStart'
+import onWorkerResponseClientId from './events/onWorkerResponseClientId'
+
+(async () => {
+  const foundation = new voodux.Foundation({
+    name: 'My Ionic App',
+    schemas: {
+      User: UserSchema,
+      Product: ProductSchema,
+      Order: OrderSchema,
+      Customer: CustomerSchema,
+      Drink: DrinkSchema
+    }
+  })
+  
+  const appStartListener = foundation.on('foundation:start', onApplicationStart.bind(foundation))
+  const workerSendClientIdListener = foundation.on('worker:responseClientId', onWorkerResponseClientId.bind(foundation))
+  window.addEventListener('unload', (event) => {
+    foundation.stopListenTo(appStartListener)
+    foundation.stopListenTo(workerSendClientIdListener)
+  })
+
+  const start = await foundation.start()
+  if (start.error) {
+    throw new Error(`Error starting foundation stack: ${start.error}`)
+  }
+
+  // console.debug('start', start)
+  ReactDOM.render(
+    <FoundationProvider value={foundation}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>,
+    </FoundationProvider>,
+    document.getElementById('root')
+  )
+
+
+  // If you want your app to work offline and load faster, you can change
+  // unregister() to register() below. Note this comes with some pitfalls.
+  // Learn more about service workers: https://cra.link/PWA
+  serviceWorkerRegistration.register();
+
+  // If you want to start measuring performance in your app, pass a function
+  // to log results (for example: reportWebVitals(console.log))
+  // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+  reportWebVitals();
+})()
